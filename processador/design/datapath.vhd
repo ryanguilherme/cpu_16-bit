2e33ba66177c54a808573423ed1a114f90046a18
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-05-25 19:59:25 -0300

    unidade de controle, datapath e processador (nao concluidos)

---
+++ processador/design/datapath.vhd
@@ -0,0 +1,57 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity datapath is
+	generic(N : integer := 16);
+	Port (
+		clk : in std_logic;
+		rst : in std_logic;
+		Rd_sel : in std_logic_vector(2 downto 0);
+		Rd_wr : in std_logic := '0';
+		Rm_sel : in std_logic_vector(2 downto 0);
+		Rn_sel : in std_logic_vector(2 downto 0);
+		Immed : in std_logic_vector(N-1 downto 0);
+		ula_op : in std_logic_vector(3 downto 0);
+		ram_d : in std_logic_vector(N-1 downto 0);
+		ram_q : out std_logic_vector(N-1 downto 0);
+		ram_addr : out std_logic_vector(N-1 downto 0);
+		ram_sel : in std_logic
+	);
+
+architecture Behavorial of datapath is
+	signal s_Rd : std_logic_vector(N-1 downto 0);
+	signal s_Rm : std_logic_vector(N-1 downto 0);
+	signal s_Rn : std_logic_vector(N-1 downto 0);
+	signal s_ram_q : std_logic_vector(N-1 downto 0);
+begin
+	mux_2x1 : ram_q <= s_Rn when ram_sel='0' else Immed;
+	mux_4x1 : s_Rd <= s_Rm when rf_sel="00" else
+				s_ram_q when rf_sel="01" else
+				Immed when rf_sel ="10" else
+				s_Rn;
+	RF : entity work.register_file
+		generic map(N=>16)
+		Port map(
+			clk => clk,
+			rst => rst,
+			Rd_sel => Rd_sel,
+			Rd_wr => Rd_wr,
+			Rn_sel => Rn_sel,
+			Rd => s_Rd,
+			RM => s_Rm,
+			Rn => s_Rn
+		);
+
+	ula : entity work.ula
+		generic map(N=>16)
+		Port map(
+			A => s_Rm,
+			B => s_Rn,
+			Q => s_ula_q,
+			op => ula_op
+		);
+
+	
+
+
+end Behavorial;
---
+++ processador/design/processador.vhd
@@ -0,0 +1,56 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity processador is
+	generic(N : integer := 16);
+	Port (
+		clk : std_logic;
+		rst : std_logic;
+		ROM_addr : out std_logic_vector(N-1 downto 0);
+		ROM_en : out std_logic;
+		ROM_out : in std_logic_vector(N-1 downto 0);
+		RAM_D : out std_logic_vector(N-1 downto 0);
+		RAM_Q : 
+	);
+end entity;
+
+architecture Behavorial of processador is
+
+begin
+	DP : entity work.registrador
+		generic map(N=>16)
+		Port(
+			clk => clk,
+			rst => rst,
+			Rd_sel => Rd_sel,
+			Rd_wr => Rd_wr,
+			Rm_sel => Rm_sel,
+			Rn_sel => Rn_sel,
+			Immed => Immed		
+
+	UC : entity work.controUnit
+		generic map(N=>16)
+		Port(
+			clk => clk,
+			rst => rst,
+			Ir_data => Ir_data,
+			ROM_en => ROM_en,
+			ROM_addr => ROM_addr,
+			Immed => Immed,
+			RAM_sel => RAM_sel,
+			RAM_we => RAM_we,
+			RF_sel => RF_sel,
+			Rd_sel => Rd_sel,
+			Rd_wr => Rd_wr,
+			Rm_sel => Rm_sel,
+			
+
+end Behavorial;
+
+
+
+
+
+
+
