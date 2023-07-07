d945b4bfc6205e6a26b1e171aef734143ea16b07
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-07-06 22:41:25 -0300

    máquina de estados atualizada para novas funcionalidades de jump, e adicionadas decodificações para novas instruções da ula e de salto

--- 16bits_cpu/design/16bits_cpu/16bits_cpu.srcs/sources_1/new/fsm.vhd
+++ 16bits_cpu/design/16bits_cpu/16bits_cpu.srcs/sources_1/new/fsm.vhd
@@ -13,28 +13,34 @@ entity fsm is
     );
     Port 
     (
-        clk     : std_logic;                            -- clock
-        rst     : std_logic;                            -- reset
-        ROM_en  : out std_logic;                        -- enable ROM
-        PC_clr  : out std_logic;                        -- reset PC
-        PC_inc  : out std_logic;                        -- PC increment (+2)
-        IR_load : out std_logic;                        -- enable IR to load instruction from ROM
-        IR_data : in  std_logic_vector(N-1 downto 0);   -- instruction data received from ROM (16 bits)
-        Immed   : out std_logic_vector(N-1 downto 0);   -- Immediate value
-        RAM_sel : out std_logic;                        -- RAM value selector (immediate or register)
-        RAM_we  : out std_logic;                        -- enable RAM write
-        RF_sel  : out std_logic_vector(1 downto 0);     -- select Register File Rd input
-        Rd_sel  : out std_logic_vector(2 downto 0);     -- Register file Rd selector
-        Rd_wr   : out std_logic;                        -- Register file Rd enable
-        Rm_sel  : out std_logic_vector(2 downto 0);     -- Register file Rm selector
-        Rn_sel  : out std_logic_vector(2 downto 0);     -- Register file Rn selector
-        ula_op  : out std_logic_vector(3 downto 0);      -- ULA operation
-        state  : out std_logic_vector(3 downto 0)      -- ULA operation
+        clk      : std_logic;                            -- clock
+        rst      : std_logic;                            -- reset
+        zero     : out std_logic;                        -- ZERO flag
+        carry    : out std_logic;                        -- CARRY flag
+        ROM_en   : out std_logic;                        -- enable ROM
+        PC_clr   : out std_logic;                        -- reset PC
+        PC_inc   : out std_logic;                        -- PC increment (+2)
+        IR_load  : out std_logic;                        -- enable IR to load instruction from ROM
+        IR_data  : in  std_logic_vector(N-1 downto 0);   -- instruction data received from ROM (16 bits)
+        Immed    : out std_logic_vector(N-1 downto 0);   -- Immediate value
+        RAM_sel  : out std_logic;                        -- RAM value selector (immediate or register)
+        RAM_we   : out std_logic;                        -- enable RAM write
+        stack_en : out std_logic;                        -- enable RAM Stack
+        stack_op : out std_logic_vector(1 downto 0);     -- RAM Stack operator (01 - PUSH, 10 - POP)
+        RF_sel   : out std_logic_vector(1 downto 0);     -- select Register File Rd input
+        Rd_sel   : out std_logic_vector(2 downto 0);     -- Register file Rd selector
+        Rd_wr    : out std_logic;                        -- Register file Rd enable
+        Rm_sel   : out std_logic_vector(2 downto 0);     -- Register file Rm selector
+        Rn_sel   : out std_logic_vector(2 downto 0);     -- Register file Rn selector
+        ula_op   : out std_logic_vector(3 downto 0);     -- ULA operation
+        state    : out std_logic_vector(3 downto 0);     -- state debug port
+        jump_en  : out std_logic;                        -- JUMP enable
+        jump_op  : out std_logic_vector(1 downto 0)      -- JUMP Operator (00 - JMP; 01 - JEQ; 10 - JLT; 11 - JGT)
     );
 end fsm;
 
 architecture Behavioral of fsm is
-    type states is (init, fetch, decode, exec_nop, exec_halt, exec_mov, exec_load, exec_store, exec_ula);
+    type states is (init, fetch, decode, exec_nop, exec_halt, exec_mov, exec_load, exec_store, exec_ula, exec_jump);
     signal NS, PS : states;
     
     signal instruction : std_logic_vector(N-1 downto 0);
@@ -56,52 +62,64 @@ begin
     begin
         case PS is
             when init  =>
-                PC_clr  <= '1';
-                PC_inc  <= '0';
-                ROM_en  <= '0';
-                IR_load <= '0';
-                Immed   <= x"0000";
-                RAM_sel <= '0';
-                RAM_we  <= '0';
-                RF_sel  <= "00";
-                Rd_sel  <= "000";
-                Rd_wr   <= '0';
-                Rm_sel  <= "000";
-                Rn_sel  <= "000";
-                ula_op  <= "0000";
-                NS      <= fetch;
-                state   <= "0001";                 
+                PC_clr   <= '1';
+                PC_inc   <= '0';
+                ROM_en   <= '0';
+                IR_load  <= '0';
+                Immed    <= x"0000";
+                RAM_sel  <= '0';
+                RAM_we   <= '0';
+                stack_en <= '0';
+                stack_op <= "00";
+                RF_sel   <= "00";
+                Rd_sel   <= "000";
+                Rd_wr    <= '0';
+                Rm_sel   <= "000";
+                Rn_sel   <= "000";
+                ula_op   <= "0000";
+                jump_en  <= '0';
+                jump_op  <= "00";
+                NS       <= fetch;
+                state    <= "0001";                 
             when fetch  =>
-                PC_clr  <= '0';
-                PC_inc  <= '1';
-                ROM_en  <= '1';
-                IR_load <= '1';
-                Immed   <= x"0000";
-                RAM_sel <= '0';
-                RAM_we  <= '0';
-                RF_sel  <= "00";
-                Rd_sel  <= "000";
-                Rd_wr   <= '0';
-                Rm_sel  <= "000";
-                Rn_sel  <= "000";
-                ula_op  <= "0000";
-                NS      <= decode;
-                state   <= "0010";
+                PC_clr   <= '0';
+                PC_inc   <= '1';
+                ROM_en   <= '1';
+                IR_load  <= '1';
+                Immed    <= x"0000";
+                RAM_sel  <= '0';
+                RAM_we   <= '0';
+                stack_en <= '0';
+                stack_op <= "00";
+                RF_sel   <= "00";
+                Rd_sel   <= "000";
+                Rd_wr    <= '0';
+                Rm_sel   <= "000";
+                Rn_sel   <= "000";
+                ula_op   <= "0000";
+                jump_en  <= '0';
+                jump_op  <= "00";
+                NS       <= decode;
+                state    <= "0010";
                              
             when decode =>
-                PC_clr  <= '0';
-                PC_inc  <= '0';
-                ROM_en  <= '0';
-                IR_load <= '0';
-                Immed   <= x"0000";
-                RAM_sel <= '0';
-                RAM_we  <= '0';
-                RF_sel  <= "00";
-                Rd_sel  <= "000";
-                Rd_wr   <= '0';
-                Rm_sel  <= "000";
-                Rn_sel  <= "000";
-                ula_op  <= "0000";
+                PC_clr   <= '0';
+                PC_inc   <= '0';
+                ROM_en   <= '0';
+                IR_load  <= '0';
+                Immed    <= x"0000";
+                RAM_sel  <= '0';
+                RAM_we   <= '0';
+                stack_en <= '0';
+                stack_op <= "00";
+                RF_sel   <= "00";
+                Rd_sel   <= "000";
+                Rd_wr    <= '0';
+                Rm_sel   <= "000";
+                Rn_sel   <= "000";
+                ula_op   <= "0000";
+                jump_en  <= '0';
+                jump_op  <= "00";
  
 -- +---------+--------+---------+----------+-----------+----------+-----------+------------+----------+
 -- | signal  |        |  0000   |   0001   |   0011    |   0100   |   0101    |    0110    |   0111   |
@@ -148,32 +166,35 @@ begin
 -- | XOR Rd, Rm, Rn | Rd = Rm xor Rn  |  ALU  | 1  0  1  0  | -  | Rd2 Rd1 Rd0 Rm2 Rm1 Rm0 Rn2 Rn1 Rn0 -   -   |
 -- +----------------+-----------------+-------+-------------+----+---------------------------------------------+
             
-            if    instruction(15 downto 0) = x"0000" then
+            if    instruction(15 downto 0) = x"0000"  then
                 NS <= exec_nop;
                 state   <= "0011";
-            elsif instruction(15 downto 0) = x"FFFF" then
+            elsif instruction(15 downto 0) = x"FFFF"  then
                 NS <= exec_halt;
                 state   <= "0100";
-            elsif instruction(15 downto 12) = "0001" then
+            elsif instruction(15 downto 12) = "0001"  then
                 NS <= exec_mov;
                 state   <= "0101";
-            elsif instruction(15 downto 12) = "0010" then
+            elsif instruction(15 downto 12) = "0010"  then
                 NS <= exec_store;
                 state   <= "0111";
-            elsif instruction(15 downto 12) = "0011" then
+            elsif instruction(15 downto 12) = "0011"  then
                 NS <= exec_load;
                 state   <= "1000";
-            elsif instruction(15 downto 12) = "0100" or     -- ADD
-                  instruction(15 downto 12) = "0101" or     -- SUB
-                  instruction(15 downto 12) = "0110" or     -- MUL
-                  instruction(15 downto 12) = "0111" or     -- AND
-                  instruction(15 downto 12) = "1000" or     -- ORR
-                  instruction(15 downto 12) = "1001" or     -- NOT
-                  instruction(15 downto 12) = "1010" or     -- XOR
-                  instruction(15 downto 12) = "1011" or     -- SHR
-                  instruction(15 downto 12) = "1100" or     -- SHL
-                  instruction(15 downto 12) = "1101" or     -- ROR
-                  instruction(15 downto 12) = "1110" then   -- ROL
+            elsif instruction(15 downto 11) = "00001" then
+                NS <= exec_jump;
+            elsif (instruction(15 downto 12) = "0000" and instruction(1 downto 0) = "11") or -- CMP
+                   instruction(15 downto 12) = "0100" or     -- ADD
+                   instruction(15 downto 12) = "0101" or     -- SUB
+                   instruction(15 downto 12) = "0110" or     -- MUL
+                   instruction(15 downto 12) = "0111" or     -- AND
+                   instruction(15 downto 12) = "1000" or     -- ORR
+                   instruction(15 downto 12) = "1001" or     -- NOT
+                   instruction(15 downto 12) = "1010" or     -- XOR
+                   instruction(15 downto 12) = "1011" or     -- SHR
+                   instruction(15 downto 12) = "1100" or     -- SHL
+                   instruction(15 downto 12) = "1101" or     -- ROR
+                   instruction(15 downto 12) = "1110" then   -- ROL
                 NS <= exec_ula;
                 state   <= "1001";
             else
@@ -234,6 +255,13 @@ begin
                 ula_op <= instruction(15 downto 12);
                 NS <= fetch;
                 state   <= "1001";
+                if (instruction(15 downto 12) = "1011" or instruction(15 downto 12) = "1100") then
+                    Immed <= x"00" & "000" & instruction(4 downto 0);
+                end if;
+            when exec_jump =>
+                    jump_en <= '1';
+                    jump_op <= instruction(1 downto 0);
+                    Immed <= x"00" & instruction(10 downto 2);
             when others =>
                 NS <= fetch;
                 state   <= "0010";
